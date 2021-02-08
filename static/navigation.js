3fe58a3e765611220a808e7da3c677bb49d98561
이도현 <2016025705@hanyang.ac.kr>
2021-02-08 21:18:45 +0900

    window 객체 미인식 해결

--- static/navigation.js
+++ static/navigation.js
@@ -4,39 +4,40 @@ var education = undefined
 var skills = undefined
 var awards = undefined
 var is_mobile_menu_active = false
-// let projects = undefined
 
 $(document).ready(function(){
+    console.log("READY")
     about = $('.about').position().top;
     experience = $('.experience').position().top;
     education = $('.education').position().top;
     skills = $('.skills').position().top;
     awards = $('.awards').position().top;
-    // projects = $('.projects').position().top;
-})
 
-$(window).scroll(function(){
-    var scroll = $(window).scrollTop();
-    if (0 < scroll && scroll < experience) {
-        $('.nav_experience').removeClass("nav-active")
-        $('.nav_about').addClass("nav-active")
-    } else if (experience <= scroll && scroll < education) {
-        $('.nav_about').removeClass("nav-active")
-        $('.nav_experience').addClass("nav-active")
-        $('.nav_education').removeClass("nav-active")
-    } else if (education <= scroll && scroll < skills) {
-        $('.nav_experience').removeClass("nav-active")
-        $('.nav_education').addClass("nav-active")
-        $('.nav_skills').removeClass("nav-active")
-    } else if (skills <= scroll && scroll < awards) {
-        $('.nav_education').removeClass("nav-active")
-        $('.nav_skills').addClass("nav-active")
-        $('.nav_awards').removeClass("nav-active")
-    } else if (awards <= scroll) {
-        $('.nav_skills').removeClass("nav-active")
-        $('.nav_awards').addClass("nav-active")
-        // $('.nav_projects').removeClass("nav-active")
-    } 
+    $(window).scroll(function(){
+        console.log("!!")
+        var scroll = $(window).scrollTop();
+        console.log(scroll)
+        if (0 < scroll && scroll < experience) {
+            $('.nav_experience').removeClass("nav-active")
+            $('.nav_about').addClass("nav-active")
+        } else if (experience <= scroll && scroll < education) {
+            $('.nav_about').removeClass("nav-active")
+            $('.nav_experience').addClass("nav-active")
+            $('.nav_education').removeClass("nav-active")
+        } else if (education <= scroll && scroll < skills) {
+            $('.nav_experience').removeClass("nav-active")
+            $('.nav_education').addClass("nav-active")
+            $('.nav_skills').removeClass("nav-active")
+        } else if (skills <= scroll && scroll < awards) {
+            $('.nav_education').removeClass("nav-active")
+            $('.nav_skills').addClass("nav-active")
+            $('.nav_awards').removeClass("nav-active")
+        } else if (awards <= scroll) {
+            $('.nav_skills').removeClass("nav-active")
+            $('.nav_awards').addClass("nav-active")
+        } 
+    })
+
 })
 
 function move_nav(c) {
